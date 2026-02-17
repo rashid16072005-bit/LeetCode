202d02172171f7bfb1cec5450368bc579ad25103
rashid16072005-bit <rashid16072005@gmail.com>
2026-02-17 19:42:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int solve(int num){
-        int res = 0;
-        while(num!=0){
-            int r = num%10;
-            res += r;
-            num /= 10;
-        }
-        return res;
-    }
+    // int solve(int num){
+    //     int res = 0;
+    //     while(num!=0){
+    //         int r = num%10;
+    //         res += r;
+    //         num /= 10;
+    //     }
+    //     return res;
+    // }
     int addDigits(int num) {
-        if(num<10) return num;
-        //int res = 0;
-        while(num>9){
-            num = solve(num);
-        }
-        return num;
+        // if(num<10) return num;
+        // //int res = 0;
+        // while(num>9){
+        //     num = solve(num);
+        // }
+        // return num;
+
+        if (num == 0)
+            return 0;
+        else
+            return 1 + (num - 1) % 9;
     }
 };
