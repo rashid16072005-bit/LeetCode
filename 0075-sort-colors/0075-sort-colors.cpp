506961e1b138205ec8d94602b4c5e6b00456e5a1
rashid16072005-bit <rashid16072005@gmail.com>
2026-08-02 15:29:21 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.49%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int lo=0,mid=0,hi=n-1;
+        while(mid<=hi){
+            if(nums[mid]==2){
+                swap(nums[mid],nums[hi]);
+                hi--;
+                //mid++;
+            }
+            else if(nums[mid]==0){
+                swap(nums[mid],nums[lo]);
+                mid++;
+                lo++;
+            }
+            else mid++;
+        }
+        
+        
+    }
+};
